c00ba54498aad8705691da72e9e08dcbeb286692
rentsomechairs <rentsomechairs@gmail.com>
2025-12-16 16:33:58 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,5 +8,5 @@ export const CONFIG = {
   GOOGLE_CLIENT_ID: "328251555874-15b0ha0ptqme9l6hoggt9kecdvd1npqb.apps.googleusercontent.com",
 
   // If true (or if APPS_SCRIPT_URL is blank), the site runs in mock mode (local demo only)
-  MOCK_MODE: true,
+  MOCK_MODE: false,
 };
