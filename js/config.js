5a074e975327bdce603b5ce5b636e840f9dec223
rentsomechairs <rentsomechairs@gmail.com>
2025-12-16 19:42:23 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 // 2) Set MOCK_MODE=false to use the real backend
 export const CONFIG = {
   MOCK_MODE: true,
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzgK8BwnJCgb6cw_w12lYz7HP0j128mBjLywHV-2V0bKuMXMaX7eqNb1b3ibmUBkOqy8w/exec",
   GOOGLE_CLIENT_ID: "328251555874-15b0ha0ptqme9l6hoggt9kecdvd1npqb.apps.googleusercontent.com"
 };
